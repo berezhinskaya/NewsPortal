1d77db159b718df197467c561829bb203fdabb88
berezhinskaya <bereginskaya1@gmail.com>
2026-05-15 22:43:13 +0300

    Update Program.cs

--- NewsPortal/Program.cs
+++ NewsPortal/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlite("Data Source=news.db"));
+   options.UseSqlite("Data Source=db/news.db"));
 
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
